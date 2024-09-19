c05ffc8028e03564ad4ca240b3882e08cb265961
Alexander Ray Marcum <a.ray.marcum@gmail.com>
2024-09-19 14:14:36 -0700

    Update tensors-practice.py

--- tensors-practice.py
+++ tensors-practice.py
@@ -1,8 +1,14 @@
 import tensorflow as tf
 
+def getVersion():
+  #Prints the current version of TensorFlow
+  print(tf.__versions__)
+  
 def scalar(x):
+  #Prints out some details about a Scalar
   print('This is a Scalar. A Scalar is a 0-Dimensional Tensor')
   scalar = tf.constant(x)
   scalar
-  acalar.ndim
+  print('Notice the number of dimensions of the scalar when we call the "ndim" function against it: ' + scalar.ndim)
+
 
