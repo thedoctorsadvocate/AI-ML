f2fec73aa98eae3a47cd801af0d0a2a0c60ff931
Alexander Ray Marcum <a.ray.marcum@gmail.com>
2024-09-19 14:24:08 -0700

    Update tensors-practice.py

--- tensors-practice.py
+++ tensors-practice.py
@@ -4,11 +4,16 @@ def getVersion():
   #Prints the current version of TensorFlow
   print(tf.__versions__)
   
-def scalar(x):
+def scalar() -> None:
   #Prints out some details about a Scalar
   print('This is a Scalar. A Scalar is a 0-Dimensional Tensor')
-  scalar = tf.constant(x)
+  scalar = tf.constant(7)
   scalar
   print('Notice the number of dimensions of the scalar when we call the "ndim" function against it: ' + scalar.ndim)
 
-
+def vector() -> None:
+  #Prints out some details about a Vector
+  print('This is a Vector. A Vector is a 1-Dimensional Tensor')
+  vector = tf.constant([1, 2])
+  vector
+  print('Notice the number of dimensions of the vector when we call the "ndim" function against it: ' + vector.ndim)
